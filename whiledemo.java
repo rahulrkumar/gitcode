7a9b6270530b2eda19184b4f19b8cd0ea46f883f
rahulrkumar <nrkrahul@gmail.com>
2017-09-07 17:41:54 +0530

    While loop demo

---
+++ whiledemo.java
@@ -0,0 +1,18 @@
+class whiledemo {
+
+	public static void main(String[] args) {
+
+		int rrk = 20;
+
+		System.out.println("How you doing mister ? get back to business");
+
+		while ( rrk > 0){
+
+			rrk --;
+
+			System.out.println("Backwards :" +rrk);
+
+			
+		}
+	}
+}
