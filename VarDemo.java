40c0825fd837fb07f9f35fe36254e15dc20674f6
rahulrkumar <nrkrahul@gmail.com>
2017-09-07 12:32:44 +0530

    Variables Demo

---
+++ VarDemo.java
@@ -0,0 +1,16 @@
+class VarDemo {
+
+	public static void main(String[] args){
+
+		int rrk = 50; 
+
+		int google = 75;
+
+		int apple = 67;
+
+		System.out.println(google);
+
+		System.out.println("I just typed the value of google");
+		
+	}
+}
