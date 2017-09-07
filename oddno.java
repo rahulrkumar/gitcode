857e6700621a965c3bb8395b64c5e60b85441870
rahulrkumar <nrkrahul@gmail.com>
2017-09-07 17:25:50 +0530

    Odd numbers until 10,000

---
+++ oddno.java
@@ -0,0 +1,15 @@
+class oddno {
+
+	public static void main (String[] args) {
+
+		int rrk; 
+
+		for (rrk = 1; rrk <= 100000;rrk++) {
+
+			if (rrk % 2 == 1) {
+
+				System.out.println("Odd Numbers :" + rrk);
+			}
+		}
+	}
+}
