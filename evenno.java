691f887ebfc3cdcd9daf88d863f725d8c3bb89e7
rahulrkumar <nrkrahul@gmail.com>
2017-09-07 16:46:29 +0530

    Even Numbers untill 10,000. Wow That was pretty quick, at least in my PC.

---
+++ evenno.java
@@ -0,0 +1,18 @@
+class evenno {
+
+	public static void main(String args[]) {
+
+		int rrk;
+
+		for (rrk = 2; rrk <=10000; rrk++) {
+
+			if (rrk % 2 == 0){
+
+			 System.out.println(" Even Number : " +rrk);
+
+			}
+
+			
+		}
+	}
+}
