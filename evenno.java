2dcdc5bcc1dc3a06ca4fe056b0eef6d7ae7c1493
rahulrkumar <nrkrahul@gmail.com>
2017-09-07 16:58:51 +0530

    Even numbers untill 1 Lakh with the Time how much it took to run. This is cool stuff

--- evenno.java
+++ evenno.java
@@ -2,9 +2,11 @@ class evenno {
 
 	public static void main(String args[]) {
 
+		final long startTime = System.nanoTime();
+
 		int rrk;
 
-		for (rrk = 2; rrk <=10000; rrk++) {
+		for (rrk = 2; rrk <=100000; rrk++) {
 
 			if (rrk % 2 == 0){
 
@@ -14,5 +16,11 @@ class evenno {
 
 			
 		}
-	}
+	   
+
+	   	final long duration = System.nanoTime();
+	
+	   	System.out.println(duration);
+	}   
+
 }
