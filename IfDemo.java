b68db6fd26db6b1c12a35a5158bb1b6d871a2144
rahulrkumar <nrkrahul@gmail.com>
2017-09-07 16:34:39 +0530

    If and For demosntration

---
+++ IfDemo.java
@@ -0,0 +1,21 @@
+class IfDemo{
+
+	public static void main(String[] args) {
+
+		int rrk, harman;
+
+		rrk = 34;
+
+		harman = 45;
+
+		if (rrk > harman) {
+
+			System.out.println("Yes! You are right. Good math");
+		}
+
+		else {
+
+			System.out.println("Nope you are wrong.Go back to School");
+		}
+	}
+}
---
+++ fordemo.java
@@ -0,0 +1,12 @@
+class fordemo {
+
+	public static void main(String args[]) {
+
+		int x; 
+
+		for ( x = 1; x <= 20; x++) 
+
+			System.out.println(" Numbers between 1 and 20 : " +x);
+		
+	}
+}
